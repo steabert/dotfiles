d0215799d0ec42bd6a287399e9f1f90f20ce9c61
steabert <steabert@users.noreply.github.com>
2021-03-18 21:13:12 +0100

    add more to the linker script

--- linker.sh
+++ linker.sh
@@ -3,3 +3,5 @@
 ln -sfT $PWD/gitconfig ~/.gitconfig
 ln -sfT $PWD/nvim/config ~/.config/nvim
 ln -sfT $PWD/nvim/site ~/.local/share/nvim/site
+ln -sfT $PWD/tmux.conf ~/.tmux.conf
+ln -sfT $PWD/fishconfig ~/.config/fish/config.fish
