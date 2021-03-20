31c15b43b3998ae58362a6df5031981ae81b39d4
steabert <steabert@users.noreply.github.com>
2021-03-20 17:36:56 +0100

    add full fish and sway config

--- linker.sh
+++ linker.sh
@@ -4,4 +4,5 @@ ln -sfT $PWD/gitconfig ~/.gitconfig
 ln -sfT $PWD/nvim/config ~/.config/nvim
 ln -sfT $PWD/nvim/site ~/.local/share/nvim/site
 ln -sfT $PWD/tmux.conf ~/.tmux.conf
-ln -sfT $PWD/fishconfig ~/.config/fish/config.fish
+ln -sfT $PWD/fish ~/.config/fish
+ln -sfT $PWD/sway ~/.config/sway
