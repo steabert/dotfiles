934e1757a141d0583cf1d2e6bdf482a8c23364dd
steabert <steabert@users.noreply.github.com>
2021-03-20 15:06:47 +0100

    refactor lua file

--- nvim/config/lua/lsp-init.lua
+++ nvim/config/lua/lsp-init.lua
@@ -1,23 +1,28 @@
-require('lspconfig').gopls.setup{
-	on_attach=require('completion').on_attach,
-	handlers = { 
-		["textDocument/publishDiagnostics"] = vim.lsp.with(
-		vim.lsp.diagnostic.on_publish_diagnostics, {
-			virtual_text = false
-		})
-	}
+local defaultHandlers = { 
+	["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		virtual_text = false
+	})
 }
-require('lspconfig').tsserver.setup{
-	on_attach=require('completion').on_attach,
+local attach_compl = require('completion').on_attach
+local lspconfig = require('lspconfig')
+
+-- Go
+lspconfig.gopls.setup{
+	on_attach = attach_compl,
+	handlers = defaultHandlers
+}
+
+-- TypeScript
+lspconfig.tsserver.setup{
+	on_attach = function(client)
+		client.resolved_capabilities.document_formatting = false
+		attach_compl(client)
+	end,
 	cmd={
 		"typescript-language-server",
 		"--tsserver-path", ".yarn/sdks/typescript/bin/tsserver",
 		"--stdio"
 	},
-	handlers = { 
-		["textDocument/publishDiagnostics"] = vim.lsp.with(
-		vim.lsp.diagnostic.on_publish_diagnostics, {
-			virtual_text = false
-		})
-	}
+	handlers = defaultHandlers
 }
